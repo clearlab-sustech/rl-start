13762ee1b1b6de63aaa93cad4fc5936cd9c85ba7
yinghansun <yinghansun2@gmail.com>
2024-11-06 17:28:49 +0800

    1. add constant-alpha TD. 2. off-line learning -> online learning

--- alg/temporal_difference.py
+++ alg/temporal_difference.py
@@ -1,15 +1,22 @@
 from collections import defaultdict
-
 import numpy as np
-
 
 class TdStateValueEstimator:
 
-    def __init__(self, env, gamma=1.0) -> None:
+    def __init__(
+        self, 
+        env, 
+        gamma: float = 1.0,
+        constant_alpha: bool = False,
+        alpha: float = 0.02,
+    ) -> None:
         self.env = env
         self.policy = self.create_random_policy()
         self.gamma = gamma
 
+        self.constant_alpha = constant_alpha
+        self.alpha = alpha if constant_alpha else None
+        
         self.values_table = defaultdict(float)
         self.states_count = defaultdict(int)
 
@@ -20,47 +27,52 @@ class TdStateValueEstimator:
         return policy
 
     def run_episode(self):
-        traj = []
         obs = self.env.reset()
         while True:
             action = np.random.choice(np.arange(self.env.action_dim), p=self.policy[obs])
             next_obs, reward, done, info = self.env.step(action)
-            traj.append((obs, action, reward, next_obs))
+            self.update_state_values(obs, reward, next_obs)
             obs = next_obs
 
             if done:
                 break
 
-        return traj
-    
-    def update_state_values(self, traj):
-        for t in reversed(range(len(traj))):
-            obs, action, reward, next_obs = traj[t]
-            TD_target = reward + self.gamma * self.values_table[next_obs]
-            TD_error = TD_target - self.values_table[obs]
-            
-            self.states_count[obs] += 1
-            alpha = 1 / (self.states_count[obs] + 1)
-            self.values_table[obs] += alpha * TD_error
+    def update_state_values(self, state, reward, next_state):
+        # TD(0): 1-step TD
+        TD_target = reward + self.gamma * self.values_table[next_state]
+        TD_error = TD_target - self.values_table[state]
+
+        self.states_count[state] += 1
+        if self.constant_alpha:
+            alpha = self.alpha
+        else:
+            alpha = 1 / (self.states_count[state] + 1)
+        self.values_table[state] += alpha * TD_error
 
     def estimate_state_values(self, num_episode=10000):
         for _ in range(num_episode):
-            traj = self.run_episode()
-            self.update_state_values(traj)
+            self.run_episode()
 
     @property
     def sorted_values_table(self):
+        for state in self.env.termination_states:
+            self.values_table[state] = 0.
         return tuple(sorted(self.values_table.items()))
 
 
 if __name__ == '__main__':
-    
+
     from env.frozen_lake_env import FrozenLakeEnv
 
     env = FrozenLakeEnv()
-    td_value_estimator = TdStateValueEstimator(env)
-    
-    td_value_estimator.estimate_state_values(num_episode=10000)
+    td_value_estimator = TdStateValueEstimator(
+        env,
+        gamma=0.99,
+        constant_alpha=True,
+        alpha=0.05
+    )
+
+    td_value_estimator.estimate_state_values(num_episode=20000)
 
     greedy_policy = env.compute_greedy_policy(td_value_estimator.sorted_values_table)
     print(greedy_policy)
